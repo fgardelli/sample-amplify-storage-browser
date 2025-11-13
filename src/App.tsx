50183b5efdd34cd23338c1c6924821e46c3baffa
Federico Gardelli <fgardell@amazon.it>
2025-11-13 11:01:49 +0100

    updated login names

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import './App.css';
 import config from '../amplify_outputs.json';
 import { Amplify } from 'aws-amplify';
 import { Authenticator, Button, ThemeProvider, createTheme } from '@aws-amplify/ui-react';
+import { fetchUserAttributes } from 'aws-amplify/auth';
+import { useEffect, useState } from 'react';
 Amplify.configure(config);
 
 const { StorageBrowser } = createStorageBrowser({
@@ -19,23 +21,46 @@ const theme = createTheme({
 });
 
 function App() {
+  const [userAttributes, setUserAttributes] = useState(null);
+
+  const getUserAttributes = async () => {
+    try {
+      const attributes = await fetchUserAttributes();
+      setUserAttributes(attributes);
+    } catch (error) {
+      console.error('Error fetching user attributes:', error);
+    }
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <Authenticator>
-        {({ signOut, user }) => (
-          <>
-            <div className="header">
-              <img src="/logo.png" alt="Logo" style={{ height: '40px', marginRight: '1rem' }} />
-              <h1>{`Hello ${user?.signInDetails?.loginId || user?.username}`}</h1>
-              <Button onClick={signOut}>Sign out</Button>
-            </div>
-            <div style={{ padding: '2rem' }}>
-              <h1>My WINDTRE Storage</h1>
-              <p>Managing files in your secure storage buckets</p>
-              <StorageBrowser />
-            </div>
-          </>
-        )}
+        {({ signOut, user }) => {
+          useEffect(() => {
+            if (user) {
+              getUserAttributes();
+            }
+          }, [user]);
+
+          const displayName = userAttributes?.name && userAttributes?.family_name 
+            ? `${userAttributes.name} ${userAttributes.family_name}`
+            : user?.signInDetails?.loginId || user?.username;
+
+          return (
+            <>
+              <div className="header">
+                <img src="/logo.png" alt="Logo" style={{ height: '200px', marginRight: '1rem' }} />
+                <h1>{`Hello ${displayName}`}</h1>
+                <Button onClick={signOut}>Sign out</Button>
+              </div>
+              <div style={{ padding: '2rem' }}>
+                <h1>My WINDTRE Storage</h1>
+                <p>Managing files in your secure storage buckets</p>
+                <StorageBrowser />
+              </div>
+            </>
+          );
+        }}
       </Authenticator>
     </ThemeProvider>
   );
